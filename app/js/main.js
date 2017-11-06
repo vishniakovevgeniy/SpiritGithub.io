d634770e1ac585617e98f9a979eab920b1da10b9
evgeny <vishniakovevgeniy@gmail.com>
2017-11-06 19:48:07 +0200

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+  $(".top-nav").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+        top = $(id).offset().top;
+    $("body,html").animate({scrollTop: top}, 1000);
+  });
+});
+
+$(document).ready(function() {
+    $("#scroll").on("click", "a", function(event) {
+      event.preventDefault();
+      var id = $(this).attr("href"),
+          top = $(id).offset().top;
+      $("body,html").animate({scrollTop: top}, 1500);
+    });
+  });
+
 $(document).ready(function(){
   $('.slider').bxSlider({
     mode: 'fade',
@@ -6,11 +24,30 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $('.view').hover(
-    function() {
-      $( this ).addClass('');
-    }, function() {
-      $( this ).removeClass('');
-    }
-  );
+  $(".webd").click(function(){
+    $(".photo").hide("slow");
+    $(".mobile").hide("slow");
+    $(".web").show("slow");
+  });
+});
+$(document).ready(function(){
+  $(".mobiled").click(function(){
+    $(".photo").hide("slow");
+    $(".web").hide("slow");
+    $(".mobile").show("slow");
+  });
+});
+$(document).ready(function(){
+  $(".photograpy").click(function(){
+    $(".mobile").hide("slow");
+    $(".web").hide("slow");
+    $(".photo").show("slow");
+  });
+});
+$(document).ready(function(){
+  $(".all").click(function(){
+    $(".mobile").show("slow");
+    $(".web").show("slow");
+    $(".photo").show("slow");
+  });
 });
